8cd8608d5bbc7617d4121c77583cf98c4e658865
AUTOMATED\henrylau <henrylau@asl.com.hk>
2022-11-30 09:27:44 +0800

    change

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -117,7 +117,7 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                   ),
                   Text(
-                    ' Register Now?',
+                    ' Register Now???',
                     style: TextStyle(
                       color: Colors.blue,
                       fontWeight: FontWeight.bold,
